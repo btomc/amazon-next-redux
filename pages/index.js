322c6d1151b656fd22ed38f0775290215fbc6b52
Beata Tomczak <tma.btty@gmail.com>
2021-05-24 16:49:25 +0200

    Fetch data from fake store api

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import ProductFeed from '../components/ProductFeed'
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <div className='bg-gray-100'>
       <Head>
@@ -14,11 +15,22 @@ export default function Home() {
       <Header />
 
       <main className='max-w-screen-2xl mx-auto'>
-        {/* Banner */}
         <Banner />
 
-        {/* ProductFeed */}
+        <ProductFeed products={products} />
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch('https://fakestoreapi.com/products').then(
+    (res) => res.json()
+  )
+
+  return {
+    props: {
+      products,
+    },
+  }
 }
