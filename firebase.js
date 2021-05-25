852b75eb1db7c61cefeb236335e868c262d38258
Beata Tomczak <tma.btty@gmail.com>
2021-05-25 23:54:59 +0200

    Implement firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD2dYIQXoXd4GydywdlyCLTDux7RndC6IA',
+  authDomain: 'amaz-next.firebaseapp.com',
+  projectId: 'amaz-next',
+  storageBucket: 'amaz-next.appspot.com',
+  messagingSenderId: '67216687074',
+  appId: '1:67216687074:web:4e19c5d36224755e0d83bb',
+}
