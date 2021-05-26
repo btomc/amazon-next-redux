2d6fcb024a629e14efc05765faea1a24a34d577d
Beata Tomczak <tma.btty@gmail.com>
2021-05-27 00:15:15 +0200

    Checkout Page - left side styles

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Header from '../components/Header'
 
 function Checkout() {
@@ -7,6 +8,18 @@ function Checkout() {
 
       <main className='lg:flex max-w-screen-2xl mx-auto'>
         {/* Left */}
+        <div className='flex-grow m-5 shadow-sm'>
+          <Image
+            src='/images/banner.png'
+            width={1020}
+            height={250}
+            objectFit='contain'
+          />
+
+          <div className='flex flex-col p-5 space-y-10 bg-white'>
+            <h1 className='text-3xl border-b pb-4'>Your Shopping Basket</h1>
+          </div>
+        </div>
 
         {/* Right */}
       </main>
