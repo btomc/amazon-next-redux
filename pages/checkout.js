57040675fc376cfb8bc6e7ebfee055be23ae5ec5
Beata Tomczak <tma.btty@gmail.com>
2021-05-31 23:33:03 +0200

    Create the Checkout Session

--- pages/checkout.js
+++ pages/checkout.js
@@ -6,6 +6,7 @@ import { selectItems, selectTotal } from '../slices/basketSlice'
 import Currency from 'react-currency-formatter'
 import { useSession } from 'next-auth/client'
 import { loadStripe } from '@stripe/stripe-js'
+import axios from 'axios'
 
 const stripePromise = loadStripe(process.env.stripe_public_key)
 
@@ -14,7 +15,24 @@ function Checkout() {
   const items = useSelector(selectItems)
   const total = useSelector(selectTotal)
 
-  const createCheckoutSession = () => {}
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise
+
+    // Call the backend to create a checkout session
+    const checkoutSession = await axios.post('/api/create-checkout-session', {
+      items: items,
+      email: session.user.email,
+    })
+
+    // Redirect user/customer to Stripe Checkout
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    })
+
+    if (result.error) {
+      alert(result.error.message)
+    }
+  }
 
   return (
     <div className='bg-gray-100'>
