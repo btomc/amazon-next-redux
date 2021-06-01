eaf6a46a3c4fb283bb91164f4cbd87c49ffa660a
Beata Tomczak <tma.btty@gmail.com>
2021-06-01 21:44:30 +0200

    Orders Page - fetch orders & firebase db setup

--- firebase.js
+++ firebase.js
@@ -8,3 +8,11 @@ const firebaseConfig = {
   messagingSenderId: '67216687074',
   appId: '1:67216687074:web:4e19c5d36224755e0d83bb',
 }
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app()
+
+const db = app.firestore()
+
+export default db
